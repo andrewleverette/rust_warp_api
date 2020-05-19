78fcb55134220b6cc4ee20579132bf0ab5e67d79
andrewleverette <andrew.leverette@decisionsource.com>
2020-05-18 19:35:10 -0500

    Add doc comments

--- src/db.rs
+++ src/db.rs
@@ -6,8 +6,14 @@ use tokio::sync::Mutex;
 
 use crate::models::Customer;
 
+/// Represents an in memory data store of customer data
 pub type Db = Arc<Mutex<Vec<Customer>>>;
 
+
+/// Initializes the data store
+/// 
+/// Returns a Db type that either contains customer data
+/// or is empty.
 pub fn init_db() -> Db {
     let file = File::open("./data/customers.json");
     match file {
--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,20 @@
 use serde::{Deserialize, Serialize};
 
+/// Represents a customer
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Customer {
+    /// A unique identifier for a customer record
     pub guid: String,
+
+    /// First name
     pub first_name: String,
+
+    /// Last name
     pub last_name: String,
+
+    /// Email address
     pub email: String,
+
+    /// Physical address
     pub address: String,
 }
